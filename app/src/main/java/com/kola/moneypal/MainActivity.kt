4490a282dfc2cf0abd964ba1e1002734f51f6833
tsafdagh <tsafackdcedrick@gmail.com>
2019-06-29 16:41:07 +0200

    - ajout de la creation de groupe d'objectif

--- app/src/main/java/com/kola/moneypal/MainActivity.kt
+++ app/src/main/java/com/kola/moneypal/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.kola.moneypal
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.kola.moneypal.authentification.UserprofileActivitu
 import com.kola.moneypal.fragments.HomeFragment
 import com.kola.moneypal.fragments.ObjectifGroupFragment
 import com.kola.moneypal.mes_exemple.ReadMessageActivity
+import com.kola.moneypal.utils.FireStoreUtil
+import com.kola.moneypal.utils.StorageUtil
+import kotlinx.android.synthetic.main.create_group_dialog.*
+import kotlinx.android.synthetic.main.create_group_dialog.view.*
+import org.jetbrains.anko.indeterminateProgressDialog
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,11 +79,16 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
+    var mDialogView: View? = null
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val itemId = item.itemId
         when (itemId) {
             R.id.id_menu_objectifs -> {
-                toast("objectifs")
+                toast("objectifs groupe")
+
+                createobjectiveGroup()
             }
 
             R.id.id_menu_stat -> {
@@ -81,5 +104,115 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private val RC_SELECT_IMAGE = 2
+    private val REQUESTrEADiMAGE = 1
+    private fun createobjectiveGroup() {
+        //Inflate the dialog with custom view
+        mDialogView = LayoutInflater.from(this).inflate(R.layout.create_group_dialog, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Creation du groupe")
+        //show dialog
+        val mAlertDialog = mBuilder.show()
+        //login button click of custom layout
+        mDialogView!!.imageView_profile_groupe_picture.setOnClickListener {
+
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUESTrEADiMAGE
+                )
+            } else {
+                val intent = Intent().apply {
+                    type = "image/*"
+                    action = Intent.ACTION_GET_CONTENT
+                    putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+                }
+                startActivityForResult(Intent.createChooser(intent, "Selectionnez une image"), RC_SELECT_IMAGE)
+            }
+        }
+        mDialogView!!.id_btn_creer_group.setOnClickListener {
+            //dismiss dialog
+
+            if (mDialogView!!.id_edit_nom_groupe.text.toString() != ""
+                && mDialogView!!.id_editText_description_objectif.text.toString() != ""
+                && mDialogView!!.id_editText_objectif_amount.text.toString() != ""
+            ) {
+                mAlertDialog.dismiss()
+
+                // on démare la création du groupe
+                val progressdialog = indeterminateProgressDialog(getString(R.string.pdialog_progression_en_cour))
+                FireStoreUtil.createObjectivegroupe(null, mDialogView!!.id_edit_nom_groupe.text.toString(),
+                    mDialogView!!.id_editText_description_objectif.text.toString(),
+                    mDialogView!!.id_editText_objectif_amount.text.toString().toDouble(), onComplete = { groupeId ->
+
+                        // si l'image est sélectionée on l'upload
+                        if (selectedImagePathUri != null) {
+                            StorageUtil.uploadImageGroupFromLocalFile(
+                                selectedImagePathUri!!,
+                                groupeId,
+                                onSuccess = { urlImage ->
+                                    //on met à jours l'url de l'image dans le groupe
+                                    FireStoreUtil.updateurlImageGroup(urlImage, groupeId, onComplete = {
+                                        toast("Groupe créer avec success")
+                                        progressdialog.dismiss()
+                                    })
+                                })
+                        } else {
+                            progressdialog.dismiss()
+                            toast("Groupe créer avec success")
+                        }
+                    })
+            } else {
+                mDialogView!!.id_edit_nom_groupe.error = getString(R.string.creation_group_champ_requis)
+                mDialogView!!.id_editText_description_objectif.error = getString(R.string.creation_group_champ_requis)
+                mDialogView!!.id_editText_objectif_amount.error = getString(R.string.creation_group_champ_requis)
+            }
+        }
+        //cancel button click of custom layout
+        mDialogView!!.btn_annuler_creation.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUESTrEADiMAGE) {
+            val intent = Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+                putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+            }
+            startActivityForResult(Intent.createChooser(intent, "Selectionnez une image de groupe"), RC_SELECT_IMAGE)
+        }
+
+    }
+
+    var selectedImagePathUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_SELECT_IMAGE && resultCode == Activity.RESULT_OK &&
+            data != null && data.data != null
+        ) {
+            selectedImagePathUri = data.data!!
+            val selectedImageBmp = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImagePathUri)
+
+            val outPutStream = ByteArrayOutputStream()
+            selectedImageBmp.compress(Bitmap.CompressFormat.JPEG, 90, outPutStream)
+            //selectedImageBytes = outPutStream.toByteArray()
+
+            Glide.with(this)
+                .load(selectedImagePathUri)
+                .into(mDialogView!!.imageView_profile_groupe_picture)
+        }
     }
 }
---
+++ app/src/main/java/com/kola/moneypal/utils/StorageUtil.kt
@@ -0,0 +1,77 @@
+package com.kola.moneypal.utils
+
+import android.net.Uri
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+
+object StorageUtil {
+
+    val storageRef = FirebaseStorage.getInstance().reference
+
+    /**Permet d'uploader les images dans une reference de firestore**/
+    fun uploadFromLocalFile(filePath: Uri, refStorage: String, onSuccess: (String) -> Unit) {
+        val file = filePath
+        val riversRef = storageRef.child(
+            refStorage + FirebaseAuth.getInstance().currentUser!!.phoneNumber
+        )
+
+        val uploadTask = file.let { riversRef.putFile(it) }
+
+        uploadTask.addOnFailureListener {
+
+        }.addOnSuccessListener {
+            val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation riversRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    onSuccess(downloadUri.toString())
+                } else {
+                    // Handle failures
+                    onSuccess("error")
+
+                }
+            }
+        }
+    }
+
+    /**Permet d'uploader les images dans la reference d'image de groupe de firestore**/
+    fun uploadImageGroupFromLocalFile(filePath: Uri, idGroupe: String, onSuccess: (String) -> Unit) {
+        val file = filePath
+        val riversRef = storageRef.child(
+             "objective_groupe_image/$idGroupe"
+        )
+
+        val uploadTask = file.let { riversRef.putFile(it) }
+
+        uploadTask.addOnFailureListener {
+
+        }.addOnSuccessListener {
+            val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation riversRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    onSuccess(downloadUri.toString())
+                } else {
+                    // Handle failures
+                    onSuccess("error")
+
+                }
+            }
+        }
+    }
+}
